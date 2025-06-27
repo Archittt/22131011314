103b524e67b13a3ce12eb9c4fc38e7a3fc279fc3
Archit Rathor <117975925+Archittt@users.noreply.github.com>
2025-06-27 15:01:52 +0530

    Update logger.js

--- LoggingMiddleware/logger.js
+++ LoggingMiddleware/logger.js
@@ -1,16 +1,21 @@
-const axios = require('axios');
+const axios = require("axios");
 
-async function log(stack, level, logPackage, message) {
+const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJhcmNoaXQuMjJzY3NlMTAxMTMyMUBnYWxnb3RpYXN1bml2ZXJzaXR5LmVkdS5pbiIsImV4cCI6MTc1MTAxNTQ3MCwiaWF0IjoxNzUxMDE0NTcwLCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiNThiN2ExNDMtMzVkNy00MzU1LWJlZjYtNmE5M2NlYWMzMzI2IiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoiYXJjaGl0IHJhdGhvciIsInN1YiI6IjY0YzhkMTc1LTMyNzAtNDk0NC04ZDQ1LTFlZGRhNDhkYWFmOSJ9LCJlbWFpbCI6ImFyY2hpdC4yMnNjc2UxMDExMzIxQGdhbGdvdGlhc3VuaXZlcnNpdHkuZWR1LmluIiwibmFtZSI6ImFyY2hpdCByYXRob3IiLCJyb2xsTm8iOiIyMjEzMTAxMTMxNCIsImFjY2Vzc0NvZGUiOiJNdWFndnEiLCJjbGllbnRJRCI6IjY0YzhkMTc1LTMyNzAtNDk0NC04ZDQ1LTFlZGRhNDhkYWFmOSIsImNsaWVudFNlY3JldCI6IkRXRVpLWkZZVHpOUUVWRlIifQ.gGukj0DlZpBFHHsrremqegoQvK_EHmu1ryn5nGOz37Y';
+
+const log = async (stack, level, pkg, message) => {
   try {
-    await axios.post("http://20.244.56.144/evaluation-service/logs", {
-      stack,
-      level,
-      package: logPackage,
-      message
-    });
-  } catch (error) {
-    console.error("Logging failed:", error.message);
+    const res = await axios.post(
+      "http://20.244.56.144/evaluation-service/logs",
+      { stack, level, package: pkg, message },
+      {
+        headers: {
+          Authorization: AUTH_TOKEN
+        }
+      }
+    );
+  } catch (err) {
+    console.error("Logging failed:", err.response?.data || err.message);
   }
-}
+};
 
 module.exports = log;
